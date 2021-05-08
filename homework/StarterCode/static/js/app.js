349f98d677463f50154750645b43982db9e66163
Jose Luis <jolui334@gmail.com>
2021-05-08 00:28:32 -0500

    Bar chart has been created, Bubble chart started

--- homework/StarterCode/static/js/app.js
+++ homework/StarterCode/static/js/app.js
@@ -7,8 +7,60 @@ function unpack(rows, index) {
 
 
 d3.json("samples.json").then(
-    d => {
-        let name = d.samples
-        console.log(name)
-    }
-    )
+    d => { 
+        let name = d.samples.map((x,y) => {
+        console.log(`Sample Values ${x.sample_values}`);
+        console.log(`OTU IDS ${x.otu_ids}`);       
+        console.log(`OTU labels ${x.otu_labels}`);
+        let  sValues = x.sample_values;
+        let otuIds = x.otu_ids;
+        let otuIdsList = [];  
+        x.otu_ids.forEach((a) => {otuIdsList.push("OTU "+a)});
+        let topOtus = otuIdsList.sort((a,b) => b-a).slice(0,10);        
+        let otuLabels = x.otu_labels;
+        
+
+        let barTrace = {
+          type: 'bar',
+          x: sValues,
+          y: topOtus,
+          text: otuLabels,
+          orientation: 'h',
+          transforms: [{
+            type: 'sort',
+            target: 'y',
+            order: 'descending'
+          }]
+        }
+
+
+
+
+        let data = [barTrace];
+
+        let layout = { 
+          title: `Otus find in Belly button`,
+        }
+
+        Plotly.newPlot("bar", data, layout);
+
+
+        let bubbleTrace = {
+          x: otuIds,
+          y: sValues,
+          mode: 'markers',
+          
+
+          }]
+        }
+
+
+
+
+
+
+    });
+}     
+
+);
+
